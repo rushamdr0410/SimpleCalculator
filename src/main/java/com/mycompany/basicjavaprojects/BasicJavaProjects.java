1281d0f09b867fea3138b713a30790835dac423d
rushamdr0410 <rushamanandhar3@gmail.com>
2026-06-10 12:21:47 +0545

    Day-01 of learning Java

---
+++ src/main/java/com/mycompany/basicjavaprojects/BasicJavaProjects.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.basicjavaprojects;
+
+/**
+ *
+ * @author user
+ */
+public class BasicJavaProjects {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
